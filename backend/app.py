1e0872d133c17910177f0dd8877f85bfb5918bbd
yashika641 <123154422+yashika641@users.noreply.github.com>
2025-12-22 22:05:58 +0530

    edits in requirements

--- backend/app.py
+++ backend/app.py
@@ -21,6 +21,7 @@ app.add_middleware(
     allow_origins=[
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "https://dreamy-frangollo-57d87c.netlify.app/"
 ],
     allow_credentials=True,
     allow_methods=["*"],
